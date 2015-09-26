99baa57fcaf429afd8085d079565447d46229776
LivePwndz <pwndz172@gmail.com>
2015-09-26 15:50:23 +0300

    revert router in electronics3

--- modules/electronics3/config/routes.php
+++ modules/electronics3/config/routes.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 // RMSiva Start
-//$config['RMSiva'] ="electronics1/stores_home_page/RMSiva";
+//$config['RMSiva'] ="electronics1//RMSiva";
 //$config['RMSiva/products.html'] = "/electronics1/product_list/RMSiva";
 //$config['RMSiva/today-deals.html'] = "/electronics1/deal_list/RMSiva";
 //$config['RMSiva/auction.html'] = "/electronics1/auction_list/RMSiva";
@@ -9,7 +9,7 @@
 //$config['RMSiva/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/RMSiva/$1/$2";
 //// RMSiva End
 //// UrbanMettlesdf Start
-//$config['UrbanMettlesdf'] ="electronics1/stores_home_page/UrbanMettlesdf";
+//$config['UrbanMettlesdf'] ="electronics1//UrbanMettlesdf";
 //$config['UrbanMettlesdf/products.html'] = "/electronics1/product_list/UrbanMettlesdf";
 //$config['UrbanMettlesdf/today-deals.html'] = "/electronics1/deal_list/UrbanMettlesdf";
 //$config['UrbanMettlesdf/auction.html'] = "/electronics1/auction_list/UrbanMettlesdf";
@@ -18,7 +18,7 @@
 //$config['UrbanMettlesdf/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/UrbanMettlesdf/$1/$2";
 //// UrbanMettlesdf End
 //// A-Slice-of-Brooklyn-Toursdfsdfsd Start
-//$config['A-Slice-of-Brooklyn-Toursdfsdfsd'] ="electronics1/stores_home_page/A-Slice-of-Brooklyn-Toursdfsdfsd";
+//$config['A-Slice-of-Brooklyn-Toursdfsdfsd'] ="electronics1//A-Slice-of-Brooklyn-Toursdfsdfsd";
 //$config['A-Slice-of-Brooklyn-Toursdfsdfsd/products.html'] = "/electronics1/product_list/A-Slice-of-Brooklyn-Toursdfsdfsd";
 //$config['A-Slice-of-Brooklyn-Toursdfsdfsd/today-deals.html'] = "/electronics1/deal_list/A-Slice-of-Brooklyn-Toursdfsdfsd";
 //$config['A-Slice-of-Brooklyn-Toursdfsdfsd/auction.html'] = "/electronics1/auction_list/A-Slice-of-Brooklyn-Toursdfsdfsd";
@@ -27,7 +27,7 @@
 //$config['A-Slice-of-Brooklyn-Toursdfsdfsd/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/A-Slice-of-Brooklyn-Toursdfsdfsd/$1/$2";
 //// A-Slice-of-Brooklyn-Toursdfsdfsd End
 //// ABC-Stores Start
-//$config['ABC-Stores'] ="electronics1/stores_home_page/ABC-Stores";
+//$config['ABC-Stores'] ="electronics1//ABC-Stores";
 //$config['ABC-Stores/products.html'] = "/electronics1/product_list/ABC-Stores";
 //$config['ABC-Stores/today-deals.html'] = "/electronics1/deal_list/ABC-Stores";
 //$config['ABC-Stores/auction.html'] = "/electronics1/auction_list/ABC-Stores";
@@ -36,7 +36,7 @@
 //$config['ABC-Stores/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/ABC-Stores/$1/$2";
 //// ABC-Stores End
 //// AKS-Stores22 Start
-//$config['AKS-Stores22'] ="electronics1/stores_home_page/AKS-Stores22";
+//$config['AKS-Stores22'] ="electronics1//AKS-Stores22";
 //$config['AKS-Stores22/products.html'] = "/electronics1/product_list/AKS-Stores22";
 //$config['AKS-Stores22/today-deals.html'] = "/electronics1/deal_list/AKS-Stores22";
 //$config['AKS-Stores22/auction.html'] = "/electronics1/auction_list/AKS-Stores22";
@@ -45,7 +45,7 @@
 //$config['AKS-Stores22/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/AKS-Stores22/$1/$2";
 //// AKS-Stores22 End
 //// Testing-the-stores Start
-//$config['Testing-the-stores'] ="electronics1/stores_home_page/Testing-the-stores";
+//$config['Testing-the-stores'] ="electronics1//Testing-the-stores";
 //$config['Testing-the-stores/products.html'] = "/electronics1/product_list/Testing-the-stores";
 //$config['Testing-the-stores/today-deals.html'] = "/electronics1/deal_list/Testing-the-stores";
 //$config['Testing-the-stores/auction.html'] = "/electronics1/auction_list/Testing-the-stores";
@@ -54,7 +54,7 @@
 //$config['Testing-the-stores/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/Testing-the-stores/$1/$2";
 //// Testing-the-stores End
 //// Testtttee Start
-//$config['Testtttee'] ="electronics1/stores_home_page/Testtttee";
+//$config['Testtttee'] ="electronics1//Testtttee";
 //$config['Testtttee/products.html'] = "/electronics1/product_list/Testtttee";
 //$config['Testtttee/today-deals.html'] = "/electronics1/deal_list/Testtttee";
 //$config['Testtttee/auction.html'] = "/electronics1/auction_list/Testtttee";
@@ -63,7 +63,7 @@
 //$config['Testtttee/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/Testtttee/$1/$2";
 //// Testtttee End
 //// tester-kalai-stores Start
-//$config['tester-kalai-stores'] ="electronics1/stores_home_page/tester-kalai-stores";
+//$config['tester-kalai-stores'] ="electronics1//tester-kalai-stores";
 //$config['tester-kalai-stores/products.html'] = "/electronics1/product_list/tester-kalai-stores";
 //$config['tester-kalai-stores/today-deals.html'] = "/electronics1/deal_list/tester-kalai-stores";
 //$config['tester-kalai-stores/auction.html'] = "/electronics1/auction_list/tester-kalai-stores";
@@ -72,7 +72,7 @@
 //$config['tester-kalai-stores/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/tester-kalai-stores/$1/$2";
 //// tester-kalai-stores End
 //// AKS-Stores22 Start
-//$config['AKS-Stores22'] ="electronics1/stores_home_page/AKS-Stores22";
+//$config['AKS-Stores22'] ="electronics1//AKS-Stores22";
 //$config['AKS-Stores22/products.html'] = "/electronics1/product_list/AKS-Stores22";
 //$config['AKS-Stores22/today-deals.html'] = "/electronics1/deal_list/AKS-Stores22";
 //$config['AKS-Stores22/auction.html'] = "/electronics1/auction_list/AKS-Stores22";
@@ -81,7 +81,7 @@
 //$config['AKS-Stores22/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/AKS-Stores22/$1/$2";
 //// AKS-Stores22 End
 //// Testing-the-stores Start
-//$config['Testing-the-stores'] ="electronics1/stores_home_page/Testing-the-stores";
+//$config['Testing-the-stores'] ="electronics1//Testing-the-stores";
 //$config['Testing-the-stores/products.html'] = "/electronics1/product_list/Testing-the-stores";
 //$config['Testing-the-stores/today-deals.html'] = "/electronics1/deal_list/Testing-the-stores";
 //$config['Testing-the-stores/auction.html'] = "/electronics1/auction_list/Testing-the-stores";
@@ -90,7 +90,7 @@
 //$config['Testing-the-stores/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/Testing-the-stores/$1/$2";
 //// Testing-the-stores End
 //// A-Slice-of-Brooklyn-Toursdfsdfsd Start
-//$config['A-Slice-of-Brooklyn-Toursdfsdfsd'] ="electronics1/stores_home_page/A-Slice-of-Brooklyn-Toursdfsdfsd";
+//$config['A-Slice-of-Brooklyn-Toursdfsdfsd'] ="electronics1//A-Slice-of-Brooklyn-Toursdfsdfsd";
 //$config['A-Slice-of-Brooklyn-Toursdfsdfsd/products.html'] = "/electronics1/product_list/A-Slice-of-Brooklyn-Toursdfsdfsd";
 //$config['A-Slice-of-Brooklyn-Toursdfsdfsd/today-deals.html'] = "/electronics1/deal_list/A-Slice-of-Brooklyn-Toursdfsdfsd";
 //$config['A-Slice-of-Brooklyn-Toursdfsdfsd/auction.html'] = "/electronics1/auction_list/A-Slice-of-Brooklyn-Toursdfsdfsd";
@@ -99,7 +99,7 @@
 //$config['A-Slice-of-Brooklyn-Toursdfsdfsd/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/A-Slice-of-Brooklyn-Toursdfsdfsd/$1/$2";
 //// A-Slice-of-Brooklyn-Toursdfsdfsd End
 //// NIMS-STORESS Start
-//$config['NIMS-STORESS'] ="electronics1/stores_home_page/NIMS-STORESS";
+//$config['NIMS-STORESS'] ="electronics1//NIMS-STORESS";
 //$config['NIMS-STORESS/products.html'] = "/electronics1/product_list/NIMS-STORESS";
 //$config['NIMS-STORESS/today-deals.html'] = "/electronics1/deal_list/NIMS-STORESS";
 //$config['NIMS-STORESS/auction.html'] = "/electronics1/auction_list/NIMS-STORESS";
@@ -108,7 +108,7 @@
 //$config['NIMS-STORESS/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/NIMS-STORESS/$1/$2";
 //// NIMS-STORESS End
 //// Githan-Stores Start
-//$config['Githan-Stores'] ="electronics1/stores_home_page/Githan-Stores";
+//$config['Githan-Stores'] ="electronics1//Githan-Stores";
 //$config['Githan-Stores/products.html'] = "/electronics1/product_list/Githan-Stores";
 //$config['Githan-Stores/today-deals.html'] = "/electronics1/deal_list/Githan-Stores";
 //$config['Githan-Stores/auction.html'] = "/electronics1/auction_list/Githan-Stores";
@@ -117,7 +117,7 @@
 //$config['Githan-Stores/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/Githan-Stores/$1/$2";
 //// Githan-Stores End
 //// test-storeee Start
-//$config['test-storeee'] ="electronics1/stores_home_page/test-storeee";
+//$config['test-storeee'] ="electronics1//test-storeee";
 //$config['test-storeee/products.html'] = "/electronics1/product_list/test-storeee";
 //$config['test-storeee/today-deals.html'] = "/electronics1/deal_list/test-storeee";
 //$config['test-storeee/auction.html'] = "/electronics1/auction_list/test-storeee";
@@ -126,7 +126,7 @@
 //$config['test-storeee/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/test-storeee/$1/$2";
 //// test-storeee End
 //// Stop-Shop-Go Start
-//$config['Stop-Shop-Go'] ="electronics1/stores_home_page/Stop-Shop-Go";
+//$config['Stop-Shop-Go'] ="electronics1//Stop-Shop-Go";
 //$config['Stop-Shop-Go/products.html'] = "/electronics1/product_list/Stop-Shop-Go";
 //$config['Stop-Shop-Go/today-deals.html'] = "/electronics1/deal_list/Stop-Shop-Go";
 //$config['Stop-Shop-Go/auction.html'] = "/electronics1/auction_list/Stop-Shop-Go";
@@ -135,7 +135,7 @@
 //$config['Stop-Shop-Go/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/Stop-Shop-Go/$1/$2";
 //// Stop-Shop-Go End
 //// Crabtree-Evelynssdfds Start
-//$config['Crabtree-Evelynssdfds'] ="electronics1/stores_home_page/Crabtree-Evelynssdfds";
+//$config['Crabtree-Evelynssdfds'] ="electronics1//Crabtree-Evelynssdfds";
 //$config['Crabtree-Evelynssdfds/products.html'] = "/electronics1/product_list/Crabtree-Evelynssdfds";
 //$config['Crabtree-Evelynssdfds/today-deals.html'] = "/electronics1/deal_list/Crabtree-Evelynssdfds";
 //$config['Crabtree-Evelynssdfds/auction.html'] = "/electronics1/auction_list/Crabtree-Evelynssdfds";
@@ -144,7 +144,7 @@
 //$config['Crabtree-Evelynssdfds/auction/c/(.*)/(.*).html'] = "/electronics1/auction_list/Crabtree-Evelynssdfds/$1/$2";
 //// Crabtree-Evelynssdfds End
 //// Crabtree-Evelynssdfds Start
-//$config['Crabtree-Evelynssdfds'] ="electronics1/stores_home_page/Crabtree-Evelynssdfds";
+//$config['Crabtree-Evelynssdfds'] ="electronics1//Crabtree-Evelynssdfds";
 //$config['Crabtree-Evelynssdfds/products.html'] = "/electronics1/product_list/Crabtree-Evelynssdfds";
 //$config['Crabtree-Evelynssdfds/today-deals.html'] = "/electronics1/deal_list/Crabtree-Evelynssdfds";
 //$config['Crabtree-Evelynssdfds/auction.html'] = "/electronics1/auction_list/Crabtree-Evelynssdfds";
@@ -154,7 +154,7 @@
 //// Crabtree-Evelynssdfds End
 //
 //// Musty-International-Store Start
-//$config['Musty-International-Store'] ="electronics3/stores_home_page/Musty-International-Store";
+//$config['Musty-International-Store'] ="electronics3//Musty-International-Store";
 //$config['Musty-International-Store/products.html'] = "/electronics3/product_list/Musty-International-Store";
 //$config['Musty-International-Store/today-deals.html'] = "/electronics3/deal_list/Musty-International-Store";
 //$config['Musty-International-Store/auction.html'] = "/electronics3/auction_list/Musty-International-Store";
@@ -162,3 +162,5 @@
 //$config['Musty-International-Store/deal/c/(.*)/(.*).html'] = "/electronics3/deal_list/Musty-International-Store/$1/$2";
 //$config['Musty-International-Store/auction/c/(.*)/(.*).html'] = "/electronics3/auction_list/Musty-International-Store/$1/$2";
 //// Musty-International-Store End
+
+$config['Supermart'] = "electronics3/stores_home_page/*";
