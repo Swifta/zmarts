42df6b36cc0742422a753cc37978978bb143ca8b
Tolulope <Tolulope@cSharp>
2015-10-22 16:15:26 +0100

    Tolu work: progress

--- modules/fashion3/config/routes.php
+++ modules/fashion3/config/routes.php
@@ -1,21 +1,4 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 
-// test-store Start
-//$config['test-store'] ="fashion3/stores_home_page/test-store";
-//$config['test-store/products.html'] = "/fashion3/product_list/test-store";
-//$config['test-store/today-deals.html'] = "/fashion3/deal_list/test-store";
-//$config['test-store/auction.html'] = "/fashion3/auction_list/test-store";
-//$config['test-store/products/c/(.*)/(.*).html'] = "/fashion3/product_list/test-store/$1/$2";
-//$config['test-store/deal/c/(.*)/(.*).html'] = "/fashion3/deal_list/test-store/$1/$2";
-//$config['test-store/auction/c/(.*)/(.*).html'] = "/fashion3/auction_list/test-store/$1/$2";
-// test-store End
-// Musty-Test-Company Start
-//$config['Musty-Test-Company'] ="fashion3/stores_home_page/Musty-Test-Company";
-//$config['Musty-Test-Company/products.html'] = "/fashion3/product_list/Musty-Test-Company";
-//$config['Musty-Test-Company/today-deals.html'] = "/fashion3/deal_list/Musty-Test-Company";
-//$config['Musty-Test-Company/auction.html'] = "/fashion3/auction_list/Musty-Test-Company";
-//$config['Musty-Test-Company/products/c/(.*)/(.*).html'] = "/fashion3/product_list/Musty-Test-Company/$1/$2";
-//$config['Musty-Test-Company/deal/c/(.*)/(.*).html'] = "/fashion3/deal_list/Musty-Test-Company/$1/$2";
-//$config['Musty-Test-Company/auction/c/(.*)/(.*).html'] = "/fashion3/auction_list/Musty-Test-Company/$1/$2";
-// Musty-Test-Company End
+
