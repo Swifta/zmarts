ec13eee74d509577658d17bbec327b026e47664c
Musty <segsalerty@yahoo.com>
2015-10-22 11:45:03 +0100

    New to pause to pull and merge Paul's template

--- modules/fashion9/config/routes.php
+++ modules/fashion9/config/routes.php
@@ -1,10 +1,10 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
-// ABC-Pvt-Ltd Start
-$config['ABC-Pvt-Ltd'] ="fashion9/stores_home_page/ABC-Pvt-Ltd";
-$config['ABC-Pvt-Ltd/products.html'] = "/fashion9/product_list/ABC-Pvt-Ltd";
-$config['ABC-Pvt-Ltd/today-deals.html'] = "/fashion9/deal_list/ABC-Pvt-Ltd";
-$config['ABC-Pvt-Ltd/auction.html'] = "/fashion9/auction_list/ABC-Pvt-Ltd";
-$config['ABC-Pvt-Ltd/products/c/(.*)/(.*).html'] = "/fashion9/product_list/ABC-Pvt-Ltd/$1/$2";
-$config['ABC-Pvt-Ltd/deal/c/(.*)/(.*).html'] = "/fashion9/deal_list/ABC-Pvt-Ltd/$1/$2";
-$config['ABC-Pvt-Ltd/auction/c/(.*)/(.*).html'] = "/fashion9/auction_list/ABC-Pvt-Ltd/$1/$2";
-// ABC-Pvt-Ltd End
+// Musty-Electronics-Internatiional2II8 Start
+$config['Musty-Electronics-Internatiional2II8'] ="fashion9/stores_home_page/Musty-Electronics-Internatiional2II8";
+$config['Musty-Electronics-Internatiional2II8/products.html'] = "/fashion9/product_list/Musty-Electronics-Internatiional2II8";
+$config['Musty-Electronics-Internatiional2II8/today-deals.html'] = "/fashion9/deal_list/Musty-Electronics-Internatiional2II8";
+$config['Musty-Electronics-Internatiional2II8/auction.html'] = "/fashion9/auction_list/Musty-Electronics-Internatiional2II8";
+$config['Musty-Electronics-Internatiional2II8/products/c/(.*)/(.*).html'] = "/fashion9/product_list/Musty-Electronics-Internatiional2II8/$1/$2";
+$config['Musty-Electronics-Internatiional2II8/deal/c/(.*)/(.*).html'] = "/fashion9/deal_list/Musty-Electronics-Internatiional2II8/$1/$2";
+$config['Musty-Electronics-Internatiional2II8/auction/c/(.*)/(.*).html'] = "/fashion9/auction_list/Musty-Electronics-Internatiional2II8/$1/$2";
+// Musty-Electronics-Internatiional2II8 End
