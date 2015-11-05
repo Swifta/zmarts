f4813fb7a41a37d867ad2de8576ffa700155420e
LivePwndz <pwndz172@gmail.com>
2015-11-05 08:14:01 +0300

    removing unnecessary custom routes

--- modules/sasa/config/routes.php
+++ modules/sasa/config/routes.php
@@ -1,13 +1,4 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
-$config['(.*)/store-product-item-details/(.*)/(.*).html'] = "/sasa/details_product/$1/$2/$3"; 
-$config['(.*)/store-deal-item-details/(.*)/(.*).html'] = "/sasa/details_deals/$1/$2/$3"; 
-$config['(.*)/store-auction-item-details/(.*)/(.*).html'] = "/sasa/details_auction/$1/$2/$3"; 
-$config['leo_sign.html'] = '/welcome/leo_sign';
-$config['leo_login.html'] = '/welcome/leo_login';
-$config['leo_zenith.html'] = '/welcome/leo_zenith';
-$config['leo_logout.html'] = '/welcome/leo_logout';
-
-
 
 //ABC-Pvt-Ltd start
 $config['ABC-Pvt-Ltd'] ="/sasa/stores_home_page/ABC-Pvt-Ltd";
