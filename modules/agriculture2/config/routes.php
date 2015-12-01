57a4265e261670ac40500d894bf6c7234a5f8b79
Tolulope <Tolulope@cSharp>
2015-12-01 11:05:18 +0100

    Tolu's work on templates

---
+++ modules/agriculture2/config/routes.php
@@ -0,0 +1,38 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+//Musty-Electronics-Internatiional_PAUL start
+$config['Musty-Electronics-Internatiional_PAUL'] ="/agriculture2/stores_home_page/Musty-Electronics-Internatiional_PAUL";
+$config['Musty-Electronics-Internatiional_PAUL/products.html'] = "/agriculture2/product_list/Musty-Electronics-Internatiional_PAUL";
+$config['Musty-Electronics-Internatiional_PAUL/today-deals.html'] = "/agriculture2/deal_list/Musty-Electronics-Internatiional_PAUL";
+$config['Musty-Electronics-Internatiional_PAUL/auction.html'] = "/agriculture2/auction_list/Musty-Electronics-Internatiional_PAUL";
+$config['Musty-Electronics-Internatiional_PAUL/products/c/(.*)/(.*).html'] = "/agriculture2/product_list/Musty-Electronics-Internatiional_PAUL";
+$config['Musty-Electronics-Internatiional_PAUL/deal/c/(.*)/(.*).html'] = "/agriculture2/deal_list/Musty-Electronics-Internatiional_PAUL";
+$config['Musty-Electronics-Internatiional_PAUL/auction/c/(.*)/(.*).html'] = "/agriculture2/auction_list/Musty-Electronics-Internatiional_PAUL/$1/$2";
+//Musty-Electronics-Internatiional_PAUL end
+//Musty-Electronics-Inc start
+$config['Musty-Electronics-Inc'] ="/agriculture2/stores_home_page/Musty-Electronics-Inc";
+$config['Musty-Electronics-Inc/products.html'] = "/agriculture2/product_list/Musty-Electronics-Inc";
+$config['Musty-Electronics-Inc/today-deals.html'] = "/agriculture2/deal_list/Musty-Electronics-Inc";
+$config['Musty-Electronics-Inc/auction.html'] = "/agriculture2/auction_list/Musty-Electronics-Inc";
+$config['Musty-Electronics-Inc/products/c/(.*)/(.*).html'] = "/agriculture2/product_list/Musty-Electronics-Inc";
+$config['Musty-Electronics-Inc/deal/c/(.*)/(.*).html'] = "/agriculture2/deal_list/Musty-Electronics-Inc";
+$config['Musty-Electronics-Inc/auction/c/(.*)/(.*).html'] = "/agriculture2/auction_list/Musty-Electronics-Inc/$1/$2";
+//Musty-Electronics-Inc end
+//test-store start
+$config['test-store'] ="/agriculture2/stores_home_page/test-store";
+$config['test-store/products.html'] = "/agriculture2/product_list/test-store";
+$config['test-store/today-deals.html'] = "/agriculture2/deal_list/test-store";
+$config['test-store/auction.html'] = "/agriculture2/auction_list/test-store";
+$config['test-store/products/c/(.*)/(.*).html'] = "/agriculture2/product_list/test-store";
+$config['test-store/deal/c/(.*)/(.*).html'] = "/agriculture2/deal_list/test-store";
+$config['test-store/auction/c/(.*)/(.*).html'] = "/agriculture2/auction_list/test-store/$1/$2";
+//test-store end
+//ABC-Pvt-Ltd start
+$config['ABC-Pvt-Ltd'] ="/agriculture2/stores_home_page/ABC-Pvt-Ltd";
+$config['ABC-Pvt-Ltd/products.html'] = "/agriculture2/product_list/ABC-Pvt-Ltd";
+$config['ABC-Pvt-Ltd/today-deals.html'] = "/agriculture2/deal_list/ABC-Pvt-Ltd";
+$config['ABC-Pvt-Ltd/auction.html'] = "/agriculture2/auction_list/ABC-Pvt-Ltd";
+$config['ABC-Pvt-Ltd/products/c/(.*)/(.*).html'] = "/agriculture2/product_list/ABC-Pvt-Ltd";
+$config['ABC-Pvt-Ltd/deal/c/(.*)/(.*).html'] = "/agriculture2/deal_list/ABC-Pvt-Ltd";
+$config['ABC-Pvt-Ltd/auction/c/(.*)/(.*).html'] = "/agriculture2/auction_list/ABC-Pvt-Ltd/$1/$2";
+//ABC-Pvt-Ltd end
---
+++ modules/hospitality2/config/routes.php
@@ -0,0 +1,37 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+// Slot-Systems-Limited Start
+$config['Slot-Systems-Limited'] ="hospitality2/stores_home_page/Slot-Systems-Limited";
+$config['Slot-Systems-Limited/products.html'] = "/hospitality2/product_list/Slot-Systems-Limited";
+$config['Slot-Systems-Limited/today-deals.html'] = "/hospitality2/deal_list/Slot-Systems-Limited";
+$config['Slot-Systems-Limited/auction.html'] = "/hospitality2/auction_list/Slot-Systems-Limited";
+$config['Slot-Systems-Limited/products/c/(.*)/(.*).html'] = "/hospitality2/product_list/Slot-Systems-Limited/$1/$2";
+$config['Slot-Systems-Limited/deal/c/(.*)/(.*).html'] = "/hospitality2/deal_list/Slot-Systems-Limited/$1/$2";
+$config['Slot-Systems-Limited/auction/c/(.*)/(.*).html'] = "/hospitality2/auction_list/Slot-Systems-Limited/$1/$2";
+// Slot-Systems-Limited End
+// fdgh Start
+$config['fdgh'] ="hospitality2/stores_home_page/fdgh";
+$config['fdgh/products.html'] = "/hospitality2/product_list/fdgh";
+$config['fdgh/today-deals.html'] = "/hospitality2/deal_list/fdgh";
+$config['fdgh/auction.html'] = "/hospitality2/auction_list/fdgh";
+$config['fdgh/products/c/(.*)/(.*).html'] = "/hospitality2/product_list/fdgh/$1/$2";
+$config['fdgh/deal/c/(.*)/(.*).html'] = "/hospitality2/deal_list/fdgh/$1/$2";
+$config['fdgh/auction/c/(.*)/(.*).html'] = "/hospitality2/auction_list/fdgh/$1/$2";
+// fdgh End
+// CC-Collections Start
+$config['CC-Collections'] ="hospitality2/stores_home_page/CC-Collections";
+$config['CC-Collections/products.html'] = "/hospitality2/product_list/CC-Collections";
+$config['CC-Collections/today-deals.html'] = "/hospitality2/deal_list/CC-Collections";
+$config['CC-Collections/auction.html'] = "/hospitality2/auction_list/CC-Collections";
+$config['CC-Collections/products/c/(.*)/(.*).html'] = "/hospitality2/product_list/CC-Collections/$1/$2";
+$config['CC-Collections/deal/c/(.*)/(.*).html'] = "/hospitality2/deal_list/CC-Collections/$1/$2";
+$config['CC-Collections/auction/c/(.*)/(.*).html'] = "/hospitality2/auction_list/CC-Collections/$1/$2";
+// CC-Collections End
+// Slot-Systems-Limited Start
+$config['Slot-Systems-Limited'] ="hospitality2/stores_home_page/Slot-Systems-Limited";
+$config['Slot-Systems-Limited/products.html'] = "/hospitality2/product_list/Slot-Systems-Limited";
+$config['Slot-Systems-Limited/today-deals.html'] = "/hospitality2/deal_list/Slot-Systems-Limited";
+$config['Slot-Systems-Limited/auction.html'] = "/hospitality2/auction_list/Slot-Systems-Limited";
+$config['Slot-Systems-Limited/products/c/(.*)/(.*).html'] = "/hospitality2/product_list/Slot-Systems-Limited/$1/$2";
+$config['Slot-Systems-Limited/deal/c/(.*)/(.*).html'] = "/hospitality2/deal_list/Slot-Systems-Limited/$1/$2";
+$config['Slot-Systems-Limited/auction/c/(.*)/(.*).html'] = "/hospitality2/auction_list/Slot-Systems-Limited/$1/$2";
+// Slot-Systems-Limited End
