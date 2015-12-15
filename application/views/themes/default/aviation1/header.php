6185f392b0c2d8233baf9d3b003b74bd9e56ad85
Tolulope <tolsoft@yahoo.com>
2015-12-15 10:21:41 +0100

    some commits made 14/12/2015 9:59am: Tolu

--- application/views/themes/default/aviation1/header.php
+++ application/views/themes/default/aviation1/header.php
@@ -259,21 +259,13 @@ $srch = $this->Lang['SRCH_STR'];
 <div class="pull-right span4">
                 <div class="merchant_log text-center pull-left" style="margin:1px auto;">
              <!--<p class="text-center"><?php echo $this->Lang['MERCHANT_ACC']; ?></p>-->
-                    <?php if(empty($this->UserID)){?>
+                    
             <ul>
                 <li><p>Customer</p></li>
                     <li><a  href="javascript:showlogin();" title="Customer Login">Login</a></li>
 				<li>|</li>
 				<li><a  href="javascript:showsignup();" title="Customer Signup">Register</a></li>
             </ul>
-                    <?php  }else {?>
-                    <ul>
-                <li><p>Customer</p></li>
-                    <li><a  href="javascript:showlogin();" title="Customer Login">Login</a></li>
-				<li>|</li>
-				<li><a  href="javascript:showsignup();" title="Customer Signup">Register</a></li>
-            </ul>
-                   <?php }>
 	</div>
                  <ul class="nav topcart pull-right">
                     <li class="dropdown hover carticon "> <a href="<?php echo PATH; ?>cart.html"  title="<?php echo $this->Lang['CART']; ?>(<?php
