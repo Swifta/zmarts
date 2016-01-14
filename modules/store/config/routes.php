ff2ac024ac3dfeee3064fd6807d0e7079419e0af
dupsykool <ladmodupe@gmail.com>
2016-01-14 11:23:39 +0300

    update source code with chat features_v6iii

---
+++ modules/store/config/routes.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+// Neocourts-Hotel Start
+$config['Neocourts-Hotel'] ="store/stores_home_page/Neocourts-Hotel";
+$config['Neocourts-Hotel/products.html'] = "/store/product_list/Neocourts-Hotel";
+$config['Neocourts-Hotel/today-deals.html'] = "/store/deal_list/Neocourts-Hotel";
+$config['Neocourts-Hotel/auction.html'] = "/store/auction_list/Neocourts-Hotel";
+$config['Neocourts-Hotel/products/c/(.*)/(.*).html'] = "/store/product_list/Neocourts-Hotel/$1/$2";
+$config['Neocourts-Hotel/deal/c/(.*)/(.*).html'] = "/store/deal_list/Neocourts-Hotel/$1/$2";
+$config['Neocourts-Hotel/auction/c/(.*)/(.*).html'] = "/store/auction_list/Neocourts-Hotel/$1/$2";
+// Neocourts-Hotel End
+
