57f9345f258eec7cc721f5dbc7db8bd567537aca
dupsykool <ladmodupe@gmail.com>
2016-01-14 17:04:14 +0300

    update source code with chat features_v6iii

--- modules/store/config/routes.php
+++ modules/store/config/routes.php
@@ -10,3 +10,12 @@ $config['Neocourts-Hotel/deal/c/(.*)/(.*).html'] = "/store/deal_list/Neocourts-H
 $config['Neocourts-Hotel/auction/c/(.*)/(.*).html'] = "/store/auction_list/Neocourts-Hotel/$1/$2";
 // Neocourts-Hotel End
 
+// test-store Start
+$config['test-store'] = "store/stores_home_page/test-store";
+$config['test-store/products.html'] = "/store/product_list/test-store";
+$config['test-store/today-deals.html'] = "/store/deal_list/test-store";
+$config['test-store/auction.html'] = "/store/auction_list/test-store";
+$config['test-store/products/c/(.*)/(.*).html'] = "/store/product_list/test-store/$1/$2";
+$config['test-store/deal/c/(.*)/(.*).html'] = "/store/deal_list/test-store/$1/$2";
+$config['test-store/auction/c/(.*)/(.*).html'] = "/store/auction_list/test-store/$1/$2";
+// test-store End
