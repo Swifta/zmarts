bf4202ae7f11401363db6294f0475e886e014f65
Musty <segsalerty@yahoo.com>
2016-02-15 11:32:36 +0100

    Fixing application bug: Admin cant login trashed out

--- modules/admin/models/admin.php
+++ modules/admin/models/admin.php
@@ -153,7 +153,7 @@ class Admin_Model extends Model
 	{
 		
 		$email = strip_tags(addslashes($email));
-		$pswd = strip_tags(addslashes($email));
+		$pswd = strip_tags(addslashes($pswd));
 		
 		//$result=$this->db->query("SELECT * FROM users WHERE email = '".$email."' AND password ='".md5($pswd)."' AND user_type IN(1,7,2)");
                                         $result = $this->db->select()
@@ -161,7 +161,7 @@ class Admin_Model extends Model
                                         ->where("email = '".$email."' AND password ='".md5($pswd)."' AND user_type IN(1,7,2)")
                                         ->get();
 
-
+                               //var_dump($result);die;
                 
                 
 		//$result = $this->db->from("users")->where(array("email" => $email, "password" => md5($pswd)), "user_type" ,"IN", array(1,7))->limit(1)->get();
