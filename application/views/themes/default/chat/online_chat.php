06690925b796923808f3d1a049e43a6638c3ef66
dupsykool <ladmodupe@gmail.com>
2016-02-29 16:25:20 +0100

    - commented out chat

--- application/views/themes/default/chat/online_chat.php
+++ application/views/themes/default/chat/online_chat.php
@@ -1,12 +1,12 @@
 <?php /* chat code starts */ ?>
-<div class="live_chat">
+<!--<div class="live_chat">
     <a class="cancle" style="float:right;" href="javascript:chat_hide();" title="<?php  ?>"> <?php echo "-"; ?></a>
     <a class="live_online" href="javascript:chat_popup();"> Online Live Chat </a>
     <div class="online_options_userlist">
         <div id="online_users" class="online_users">
         </div>
     </div>
-</div>
+</div>-->
 
 <script type="text/javascript" >
 function chat_hide()
