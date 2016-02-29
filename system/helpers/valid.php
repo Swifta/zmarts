f08b4d836790b28f7fd6adc01724427874ff0cd6
LivePwndz <pwndz172@gmail.com>
2016-02-29 19:50:00 +0300

    phone validation update

--- system/helpers/valid.php
+++ system/helpers/valid.php
@@ -207,8 +207,16 @@ class valid_Core {
 	}
 	
 	public static function z_phone($number){
+		$number = trim($number);
 		if(!$number)
 		 return FALSE;
+/*$valid_numbers = array("0803", "0806", "0703", "0706", "0813", "0816", "0810", "0814", "0903", "0705", "0815", "0805", "0807", "0811", "0905", "0708", "0802", "0808 ", "0812", "0701", "0902", "0809", "0817", "0818", "0909", "0908");
+*/
+
+		$valid_numbers = array("080", "070","081","090");
+		$first_three = substr($number, 0, 3);
+		 
+		/* 
 		$zero_position = strpos($number, "0");
 		$seven_position = strpos($number, "7");
 		$eight_position = strpos($number, "8");
@@ -217,10 +225,8 @@ class valid_Core {
 			return TRUE;
 		if(($zero_position === 0) && ($eight_position === 1))
 			return TRUE;
-			
-		return FALSE;
-		
-		
+		*/
+		return in_array($first_three, $valid_numbers);
 	}
 
 	/**
